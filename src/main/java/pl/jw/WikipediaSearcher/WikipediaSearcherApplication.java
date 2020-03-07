8debda9c6e6efddfc1e1caf4531f671d7ff8f45b
jakub witkowski <jmwitkowski0@gmail.com>
2020-03-07 08:03:48 +0100

    Initial commit

---
+++ src/main/java/pl/jw/WikipediaSearcher/WikipediaSearcherApplication.java
@@ -0,0 +1,13 @@
+package pl.jw.WikipediaSearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikipediaSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikipediaSearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jw/WikipediaSearcher/WikipediaSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jw.WikipediaSearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikipediaSearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
